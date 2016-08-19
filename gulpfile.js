204e0bd4db8d90de98640d42930edbe8851f13cb
LovetsPaul <chan.narodnik1989@gmail.com>
2016-08-20 00:36:23 +0400

    new folder, gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,29 +11,47 @@ var gulp         = require('gulp'),
 gulp.task('browser-sync', ['styles', 'scripts', 'jade'], function() {
 		browserSync.init({
 				server: {
-						baseDir: "./app"
+						baseDir: "./app/_zaglushka"
 				},
 				notify: false
 		});
 		
 });
 
+// gulp.task('styles', function () {
+// 	return gulp.src('sass/*.sass')
+// 	.pipe(sass({
+// 		includePaths: require('node-bourbon').includePaths
+// 	}).on('error', sass.logError))
+// 	.pipe(rename({suffix: '.min', prefix : ''}))
+// 	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+// 	.pipe(minifycss())
+// 	.pipe(gulp.dest('app/css'))
+// 	.pipe(browserSync.stream());
+// });
+
+// gulp.task('jade', function() {
+// 	return gulp.src('jade/*.jade')
+// 	.pipe(jade({pretty:true}))
+// 	.pipe(gulp.dest('app'));
+// });
+
 gulp.task('styles', function () {
-	return gulp.src('sass/*.sass')
+	return gulp.src('sass/_zaglushka/*.sass')
 	.pipe(sass({
 		includePaths: require('node-bourbon').includePaths
 	}).on('error', sass.logError))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
 	.pipe(minifycss())
-	.pipe(gulp.dest('app/css'))
+	.pipe(gulp.dest('app/_zaglushka/css'))
 	.pipe(browserSync.stream());
 });
 
 gulp.task('jade', function() {
-	return gulp.src('jade/*.jade')
+	return gulp.src('jade/_zaglushka/*.jade')
 	.pipe(jade({pretty:true}))
-	.pipe(gulp.dest('app'));
+	.pipe(gulp.dest('app/_zaglushka'));
 });
 
 gulp.task('scripts', function() {
@@ -54,9 +72,13 @@ gulp.task('scripts', function() {
 gulp.task('watch', function () {
 	gulp.watch('sass/*.sass', ['styles']);
 	gulp.watch('jade/*.jade', ['jade']);
+	gulp.watch('sass/_zaglushka/*.sass', ['styles']);
+	gulp.watch('jade/_zaglushka/*.jade', ['jade']);
 	gulp.watch('app/libs/**/*.js', ['scripts']);
 	gulp.watch('app/js/*.js').on("change", browserSync.reload);
 	gulp.watch('app/*.html').on('change', browserSync.reload);
+
+	gulp.watch('app/_zaglushka/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
